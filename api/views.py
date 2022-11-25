a65a7911764f4b5e6b570969c14582743915f49e
Dang <ntdang_20th@student.agu.edu.vn>
2022-11-25 07:56:54 +0700

    m

--- api/views.py
+++ api/views.py
@@ -4,4 +4,4 @@ from django.views import View
 # Create your views here.
 class HomeView(View):
     def get(self, request):
-        return render(request, 'homepage/zalo_verifierJlQVCv_rBcuUW8fZsV0-GXFlXLR_a5G8Cp0.html')
+        return render(request, 'homepage/zalo_verifierNE6YEzl4C3jQqBSCcgSzBrYwfXQqnNTADJ8.html')
